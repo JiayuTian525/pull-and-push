887a9bfa408e77f9a45c2d0f08269b632a693fa9
JiayuTian525 <jiayutian2020@163.com>
2023-02-09 23:08:12 +0000

    first two rect

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 
 
 function setup() {
-  createCanvas(600*4/5, 800*4/5);
+  createCanvas(1760, 2350);
+  rectMode(CENTER);
 
 }
 
 function draw() {
   background(55);
 
-  fill(255,0,0);
-  circle(width/2,height/2,20);
+  fill(232,72,59);
+  rect(820, 250, 880, 360);//first red rect
+
+  fill(244, 178, 168);
+  rect(630, 580, 500, 300);//second pink rect
 
 }
